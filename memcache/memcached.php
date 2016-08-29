4c709c97a3262c6de5b4984ffa0fd7b5155ddca9
chenjiebin <303410541@qq.com>
2016-08-29 16:12:34 +0800

    memcache测试

--- memcache/memcached.php
+++ memcache/memcached.php
@@ -2,5 +2,28 @@
 
 $m = new Memcached();
 $m->addServer('127.0.0.1', 11211);
+
 $m->set('name', 'hauer');
 echo $m->get('name');
+
+
+$items = array(
+    'key1' => 'value1',
+    'key2' => 'value2',
+    'key3' => 'value3'
+);
+$m->setMulti($items);
+$m->getDelayed(array('key1', 'key3'), true, 'callback');
+
+function callback($memc, $item) {
+    var_dump($item);
+}
+
+$profileInfo = $m->get('user:1', 'userInfoCb');
+
+function userInfoCb($memc, $key, &$value) {
+    $profileInfo = array('user_id' => 1, 'user_name' => 'hauer');
+    $value = $profileInfo;
+    return true;
+}
+var_dump($profileInfo);
