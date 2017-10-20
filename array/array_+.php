9faf5535972cb0d04b7ba6372f6e0d51ac30c434
陈杰斌 <cjb@meitu.com>
2017-10-20 17:12:19 +0800

    测试array +

--- array/array_+.php
+++ array/array_+.php
@@ -22,40 +22,10 @@ $a = array(0 => 'a', 1 => 'b');
 $b = array('0' => 'c', '1' => 'b');
 var_dump($a + $b);
 var_dump(array_merge($a, $b));
+
+echo PHP_EOL;
+$a = array(0 => array('a'), 1 => array('b'));
+$b = array('0' => array('c'), '1' => array('b'));
+var_dump($a + $b);
+var_dump(array_merge($a, $b));
 echo '</pre>';
-//    结果
-//    array
-//        0 => string 'a' (length = 1)
-//    1 => string 'b' (length = 1)
-//    2 => string 'c' (length = 1)
-//    3 => string 'd' (length = 1)
-//
-//    array
-//        0 => string 'a' (length = 1)
-//    1 => string 'b' (length = 1)
-//
-//    array
-//        0 => string 'a' (length = 1)
-//    1 => string 'b' (length = 1)
-//    2 => string 'c' (length = 1)
-//    3 => string 'b' (length = 1)
-//
-//    array
-//        0 => string 'a' (length = 1)
-//    1 => string 'b' (length = 1)
-//
-//    array
-//        0 => string 'a' (length = 1)
-//    1 => string 'b' (length = 1)
-//    2 => string 'c' (length = 1)
-//    3 => string 'b' (length = 1)
-//
-//    array
-//        0 => string 'a' (length = 1)
-//    1 => string 'b' (length = 1)
-//
-//    array
-//        0 => string 'a' (length = 1)
-//    1 => string 'b' (length = 1)
-//    2 => string 'c' (length = 1)
-//    3 => string 'b' (length = 1)
