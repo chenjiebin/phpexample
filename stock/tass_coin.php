71d1c77fe0cacba0e9d065ecaf2de2454b63d513
陈杰斌 <cjb@meitu.com>
2019-07-11 19:29:19 +0800

    计算掷硬币的概率

--- stock/tass_coin.php
+++ stock/tass_coin.php
@@ -15,9 +15,19 @@
 
 // 10个硬币, 5个朝上的几率
 // C(10, 5) / 2^10
-echo combination(10, 5) / pow(2, 10);
+$case_count = 2;
+$total = 10;
+$sample_total = 5;
+echo calcRate($case_count, $total, $sample_total);
 echo PHP_EOL;
 
+
+/**
+ * 计算组合数C($m, $n)
+ * @param $m
+ * @param $n
+ * @return float|int
+ */
 function combination($m, $n)
 {
     $result = 1;
@@ -33,4 +43,16 @@ function combination($m, $n)
         $result = $result * array_pop($a) / array_pop($b);
     }
     return $result;
+}
+
+/**
+ * 进行若干次实验, 指定实验结果出现N次的概率
+ * @param int $case_count 出现的样例情况次数, 比如掷硬币就只有正反两种情况
+ * @param int $total 实验总次数
+ * @param int $sample_total 指定实验结果出现次数
+ * @return float
+ */
+function calcRate($case_count, $total, $sample_total)
+{
+    return combination($total, $sample_total) / pow($case_count, $total);
 }
