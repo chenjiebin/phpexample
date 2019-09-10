78dd6c9354cb5d471778ec693441b466876d44e3
chenjiebin <303410541@qq.com>
2019-09-10 10:24:00 +0800

    stock participation2

--- stock/participation2.php
+++ stock/participation2.php
@@ -1,10 +1,22 @@
 <?php
 // 计算一下分红后再买入，持有若干年后的情况
-// 这边股价不变的情况，带来的收益
-$price = 4.75; // 股价
+// 这边假设股价每年有相应变化
+
+// 1. 计算一下股价年变化率
+// 公式：(1 + 收益率) ^ 年数 * 初始股价 = 最终股价
+$start_price = 1.963; // 初始股价
+$end_price = 4.347; // 最终股价
+$year = 8; // 持有年数
+$price_year_yield_rate = ( pow($end_price / $start_price , 1/$year)  - 1);
+echo 'start_price: ' . $start_price;
+echo PHP_EOL;
+echo 'price_year_yield_rate: ' . $price_year_yield_rate;
+echo PHP_EOL;
+
+// 2. 后面开始计算每年分红后再买入的场景
+$price = $start_price; // 初始股价
 $rate = 9.12 / 100; // 股息率
 $count = 10000; // 股数
-$year = 10; // 持有年数
 $capital = $price * $count; // 本金
 echo 'capital: ' . $capital;
 echo PHP_EOL;
@@ -13,10 +25,19 @@ echo PHP_EOL;
 // 每年分红：（股数 * 股价）* 股息率
 // 除权后价格： 股价 * ( 1 - 股息率 )
 // 每年增加股数：每年分红 / 除权后价格
+echo PHP_EOL;
 for ($i = 0; $i < $year; $i++) {
+	$price = $price * (1 + $price_year_yield_rate);
 	$add_count = floor(($count * $price) * $rate / ($price * (1 - $rate)));
 	$count = $count + $add_count;
+
+	echo 'year ' . ($i + 1) . ': ' . ' price='. $price . ' count='. $count . ' add_count='. $add_count; 
+	echo PHP_EOL;
 }
+echo PHP_EOL;
+
+echo 'price: ' . $price;
+echo PHP_EOL;
 echo 'count: ' . $count;
 echo PHP_EOL;
 $total_price = ($count * $price); // 总资产
