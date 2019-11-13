f27e8e81af688c562d73106a0c9c24894d4cdf94
chenjiebin <303410541@qq.com>
2019-11-13 09:22:02 +0800

    恒生指数牛熊比计算

--- finance/800000.php
+++ finance/800000.php
@@ -11,17 +11,26 @@ $i = 0;
 foreach ($arr as $v) {
     echo ++$i;
     echo PHP_EOL;
+    if ($i <= 1) {
+        continue;
+    }
     $item = explode("\t", $v);
     $value = intval($item[5]);
     if (strpos($item['5'], 'M') !== 0) {
         $value = intval($item[5]) * 1000;
     }
+    print_r($item);
+    if ($item['6'] == '-') { // 没有成交价
+        continue;
+    }
     if ($item['2'] == '牛') {
-        $niu_xiong_bi['niu'] += $value;
+        $niu_xiong_bi['niu'] += $value * $item[6];
     } else {
-        $niu_xiong_bi['xiong'] += $value;
+        $niu_xiong_bi['xiong'] += $value * $item[6];
     }
 }
+print_r($niu_xiong_bi);
+
 echo $niu_xiong_bi['niu'] / array_sum($niu_xiong_bi) * 100;
 echo " : ";
 echo $niu_xiong_bi['xiong'] / array_sum($niu_xiong_bi) * 100;
