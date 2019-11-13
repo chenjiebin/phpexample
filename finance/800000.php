5e20320b45dcebc59f31b7c12ab65084cc2239d9
chenjiebin <303410541@qq.com>
2019-11-13 14:28:37 +0800

    恒生指数牛熊比计算

--- finance/800000.php
+++ finance/800000.php
@@ -3,7 +3,7 @@
  * 计算恒生指数的牛熊比
  */
 
-$niu_xiong_bi = ['niu' => 0, 'xiong' => 0]; // 单位k
+$niu_xiong_bi = ['niu_value' => 0, 'xiong_value' => 0]; // 单位k
 
 $content = file_get_contents('800000.txt');
 $arr = explode("\n", $content);
@@ -19,32 +19,39 @@ foreach ($arr as $v) {
     if (count($item) < 7) {
         continue;
     }
+
     $value = 0;
     if (strpos($item['5'], 'M') !== false) {
         $value = intval($item[5]) * 1000;
     } else if (strpos($item['5'], 'K') !== false) {
         $value = intval($item[5]);
     }
+    if ($item['2'] == '牛') {
+        $niu_xiong_bi['niu_value'] += $value;
+    } else {
+        $niu_xiong_bi['xiong_value'] += $value;
+    }
     echo "value:" . $value;
     echo PHP_EOL;
     if ($item['6'] == '-') { // 没有成交价
         continue;
     }
-    $money = $value * $item[6];
-    echo "money:" . $money;
-    echo PHP_EOL;
-    if ($item['2'] == '牛') {
-        $niu_xiong_bi['niu'] += $money;
-    } else {
-        $niu_xiong_bi['xiong'] += $money;
-    }
-    echo PHP_EOL;
+
+//    $money = $value * $item[6];
+//    echo "money:" . $money;
+//    echo PHP_EOL;
+//    if ($item['2'] == '牛') {
+//        $niu_xiong_bi['niu'] += $money;
+//    } else {
+//        $niu_xiong_bi['xiong'] += $money;
+//    }
+//    echo PHP_EOL;
 }
 print_r($niu_xiong_bi);
 
-echo $niu_xiong_bi['niu'] / array_sum($niu_xiong_bi) * 100;
+echo $niu_xiong_bi['niu_value'] / array_sum($niu_xiong_bi) * 100;
 echo " : ";
-echo $niu_xiong_bi['xiong'] / array_sum($niu_xiong_bi) * 100;
+echo $niu_xiong_bi['xiong_value'] / array_sum($niu_xiong_bi) * 100;
 echo PHP_EOL;
 
 
